59592033b778d505e653496f36131789871def18
sem077 <canseversemih@yahoo.com>
2020-10-16 23:58:28 +0300

    awesome feature 2 more addded

--- src/test/java/feature2.java
+++ src/test/java/feature2.java
@@ -1,5 +1,7 @@
 public class feature2 {
     public static void main(String[] args) {
         System.out.println("working on feature2");
+        System.out.println("working on feature2 more");
+        System.out.println("working on feature2 again");
     }
 }
